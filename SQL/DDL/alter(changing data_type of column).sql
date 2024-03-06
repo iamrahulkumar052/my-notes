d875e56b37730501d6e669f77cce97b18028f41f
Rahul Kumar <159034627+iamrahulkumar052@users.noreply.github.com>
2024-03-07 02:26:46 +0530

    Update DDL
    add alter for changing data_type of column

---
+++ SQL/DDL/alter(changing data_type of column).sql
@@ -0,0 +1,7 @@
+-- use this query to change data_type of column
+
+alter table student modify stud_name varchar(30);
+
+-- here,'student' is table_name
+-- 'stud_name' is name of column
+-- 'varchar(30)' is new data_type of 'stud_name'
