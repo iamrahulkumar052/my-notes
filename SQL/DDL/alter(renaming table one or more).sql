104ac30f69f37e3a77bbce51e6cb469b4a83c908
Rahul Kumar <159034627+iamrahulkumar052@users.noreply.github.com>
2024-03-07 02:32:58 +0530

    Update DDL
    add alter for renaming table

---
+++ SQL/DDL/alter(renaming table one or more).sql
@@ -0,0 +1,11 @@
+-- use this query to rename single table
+
+alter table student rename stud;
+
+-- 'student' current name for table
+-- 'stud' new name for table 
+
+
+-- use this query to rename multiple table at once
+
+rename table stud to student, school to sch;
