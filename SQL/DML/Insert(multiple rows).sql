a031879342b39295ba73c0ad947b7772b2307037
Rahul Kumar <159034627+iamrahulkumar052@users.noreply.github.com>
2024-03-09 00:20:12 +0530

    Create Insert(multiple rows).sql
    Add insert multiple rows query

---
+++ SQL/DML/Insert(multiple rows).sql
@@ -0,0 +1,13 @@
+-- use this query to insert multiple rows into a table
+
+insert into table student values(1,'rony',25,'delhi',35000),(2,'raj',26,'goa',35600),(3,'ravi',24,'mumbai',35800);
+
+-- 'student' is table_name
+-- Table structure- id,name,age,city,fees
+
+
+-- use this query to insert multiple rows into specific Column
+
+insert into table student(id,name,age) values(1,'rony',25),(2,'raj',26),(3,'ravi',24);
+
+-- in this query only id,name and age of student will enter only
