5e34d921f4f3d54c067052adbc300f788604eb45
Rahul Kumar <guptarahulboy123@gmail.com>
2024-03-08 23:45:43 +0530

    add DML insert single row query

---
+++ SQL/DML/insert(single row).sql
@@ -0,0 +1,13 @@
+-- use this query to insert single row into a table
+
+insert into table student values(1,'rony',25,'delhi',35000);
+
+-- 'student' is table_name
+-- Table structure- id,name,age,city,fees
+
+
+-- use this query to insert specific values into a table
+
+insert into table student(id,name,age) values(1,'rony',25);
+
+-- in this query only id,name and age of student will enter only
