b9c9ec01667e082eed37997169eab68de14496dd
Rahul Kumar <159034627+iamrahulkumar052@users.noreply.github.com>
2024-03-10 11:26:41 -0700

    Update DML
    add update query

---
+++ SQL/DML/update.sql
@@ -0,0 +1,7 @@
+-- use this to update specific value of table
+
+update student set stu_name = 'John' where stud_id =4
+
+-- student is table_name
+-- stu_name is column_name
+-- this will update the name of student to 'john' where stud_id=4
