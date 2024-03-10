15efb6175261afffb14980a186ca914afca6df79
Rahul Kumar <159034627+iamrahulkumar052@users.noreply.github.com>
2024-03-10 09:00:20 -0700

    Update DML
    add delete

---
+++ SQL/DML/delete.sql
@@ -0,0 +1,14 @@
+-- use this query to delete rows from table
+
+delete from student where id=4;
+
+-- 'student' is table_name
+-- it will delete row where id is 4
+
+
+
+-- use this query to delete multiple rows from table
+
+delete from student where id<4;
+
+-- it will delete rows having id 1,2,3
