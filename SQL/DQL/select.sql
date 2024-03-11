b51731a244ed46ebe3c1691d4426b221ea287aac
Rahul Kumar <guptarahulboy123@gmail.com>
2024-03-11 23:41:24 +0530

    update select.sql

--- SQL/DQL/select.sql
+++ SQL/DQL/select.sql
@@ -3,3 +3,8 @@
 select*from student;
 -- '*' means all columns from table
 -- student is table name
+
+-- use this query to fetch specific data
+
+select stud_id, stud_name from student;
+-- only 'stud_id' and 'stud_name' will fetch from table
