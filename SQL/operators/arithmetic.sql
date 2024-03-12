fe743d6798cf6fc271fc56420ab7bbee958bac6a
Rahul Kumar <guptarahulboy123@gmail.com>
2024-03-12 20:11:56 +0530

    add arithmetic operator

---
+++ SQL/operators/arithmetic.sql
@@ -0,0 +1,10 @@
+-- use this operator to perform arithmetic calculation like +,-,*,/
+
+select stud_id+100 from student;
+-- stud_id is column name while student is table
+
+select stud_id-100 from student;
+
+select stud_id*100 from student;
+
+select stud_id/100 from student;
