36b7753a4efe2ec0568ba59dc41c548e79ed939a
Rahul Kumar <guptarahulboy123@gmail.com>
2024-03-13 23:31:40 +0530

    update logical.sql

--- SQL/operators/logical.sql
+++ SQL/operators/logical.sql
@@ -3,3 +3,7 @@
 select*from student where stud_id>5 and stud_id<10;
 -- it will give stud_id from stud_id 6 to 9
 -- only if both condition is true
+
+select*from student where stud_id=5 or stud_id=10;
+-- it will give stud_id either 5 or 6, if both present then it will give both
+-- either one of them is true
