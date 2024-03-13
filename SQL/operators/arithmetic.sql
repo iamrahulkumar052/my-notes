a7a9237c2df491c7e0ed5eac9dce1c2c3e0c0795
Rahul Kumar <guptarahulboy123@gmail.com>
2024-03-13 23:40:54 +0530

    add explanation of arithmetic operator

--- SQL/operators/arithmetic.sql
+++ SQL/operators/arithmetic.sql
@@ -2,9 +2,13 @@
 
 select stud_id+100 from student;
 -- stud_id is column name while student is table
+-- it will give stud_id values with addition of 100 in it
 
 select stud_id-100 from student;
+-- it will give stud_id values with substraction of 100 in it
 
 select stud_id*100 from student;
+-- it will give stud_id values with multiplication of 100 in it
 
 select stud_id/100 from student;
+-- it will give stud_id values with division of 100 in it
