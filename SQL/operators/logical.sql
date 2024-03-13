a18db5a23875e52adcf991bb80099ecef9a321e3
Rahul Kumar <guptarahulboy123@gmail.com>
2024-03-13 23:27:54 +0530

    update logical operator

--- SQL/operators/logical.sql
+++ SQL/operators/logical.sql
@@ -1,1 +1,5 @@
 -- use this to compare two conditions at a time in 'where' clause
+
+select*from student where stud_id>5 and stud_id<10;
+-- it will give stud_id from stud_id 6 to 9
+-- only if both condition is true
