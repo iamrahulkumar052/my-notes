6329692447cf59dd1e3790fb123668f2c8bbe1db
Rahul Kumar <guptarahulboy123@gmail.com>
2024-03-16 21:22:56 +0530

    update order by clause

--- SQL/DQL/order by clause.sql
+++ SQL/DQL/order by clause.sql
@@ -1,9 +1,9 @@
 -- use this clause to sort records in ascending and descending order
 
 select * from student order by stud_age;
--- it will sort in ascending order
+-- it will sort in ascending order which default
 
-select * from student order by stud_age;
+select * from student order by stud_age asc;
 -- it will sort in ascending order
 
 select * from student order by stud_age desc;
