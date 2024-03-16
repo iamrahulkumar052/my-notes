7579556235f75dbb59c22d7855761f0a71e7e30a
Rahul Kumar <guptarahulboy123@gmail.com>
2024-03-16 21:22:08 +0530

    update order by clause

--- SQL/DQL/order by clause.sql
+++ SQL/DQL/order by clause.sql
@@ -2,3 +2,9 @@
 
 select * from student order by stud_age;
 -- it will sort in ascending order
+
+select * from student order by stud_age;
+-- it will sort in ascending order
+
+select * from student order by stud_age desc;
+-- it will sort in descending order
