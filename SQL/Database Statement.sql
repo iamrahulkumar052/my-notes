e7551f05154e5fcdee0edec369d975f36ac4e4c9
Rahul Kumar <guptarahulboy123@gmail.com>
2024-03-25 15:44:33 +0530

    add create,drop,use,show,modify for database statement

---
+++ SQL/Database Statement.sql
@@ -0,0 +1,16 @@
+-- 1. Create database
+create database rahulkumar;
+
+-- 2. Show all databases
+show databases;
+
+-- 3. Select database for use
+use rahulkumar
+
+-- 4. to delete database
+drop database demo;   -- demo is name of database
+
+-- 5. To rename database
+/*works in SQL Database Server*/
+/*Please note that this command does not work in MySQL database*/
+alter database rahulkumar modify name = testdb ;
