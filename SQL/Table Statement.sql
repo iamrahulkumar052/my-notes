b9d24e31ea98a5630eaa5a6a147831863e8fd564
Rahul Kumar <guptarahulboy123@gmail.com>
2024-03-25 16:29:12 +0530

    add more content

--- SQL/Table Statement.sql
+++ SQL/Table Statement.sql
@@ -12,22 +12,31 @@ INSERT INTO EMPLOYEE (EMP_ID,NAME,DOB,AGE,SALARY) VALUES
 (2,"John1",'2002-02-23',29,9000.5),
 (3,"John2",'2003-02-23',33,5070.04);
 
--- 3. Drop(delete) Table
+-- 3. Rename Table Name
+ALTER TABLE EMPLOYEE RENAME TO emp_details;
+
+-- 4. Change column name 
+ALTER TABLE emp_details RENAME COLUMN NAME TO EMP_NAME; 
+
+-- 5. Change column Data Types
+ALTER TABLE emp_details MODIFY NAME VARCHAR(60);
+
+-- 6. Add column into table
+ALTER TABLE emp_details ADD (CITY VARCHAR(30),STATE VARCHAR(30));
+
+-- 7. Drop Column from Table
+ALTER TABLE emp_details DROP COLUMN SALARY;
+
+-- 8. Drop(delete) Table
 DROP TABLE Student;   -- student is tablename
 
--- 4.a Delete records from Table(Single records)
+-- 9.a Delete records from Table(Single records)
 delete from EMPLOYEE where AGE=29;
 
--- 4.b Delete records from Table(ALL records)
+-- 9.b Delete records from Table(ALL records)
 delete from EMPLOYEE ;
 -- or
 delete * from EMPLOYEE;
 
--- 5. Delete all records of table [Not Table]
+-- 10. Delete all records of table [Not Table]
 TRUNCATE Table EMPLOYEE;
-
--- 5. Rename Table Name
-ALTER TABLE EMPLOYEE RENAME TO emp_details;
-
--- 6. Change column name 
-ALTER TABLE emp_details RENAME COLUMN NAME TO EMP_NAME; 
