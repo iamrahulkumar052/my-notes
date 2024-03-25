c992fc705e391865c86b6b3fc2366eed6a494220
Rahul Kumar <guptarahulboy123@gmail.com>
2024-03-25 17:17:29 +0530

    update

---
+++ SQL/Queries/UPDATE Statement.sql
@@ -0,0 +1,7 @@
+-- update single column
+UPDATE Customer SET CustomerName = 'Nitin' WHERE Age = 22;
+
+-- update multiple column
+UPDATE Customer SET CustomerName = 'Nitin', 
+Country = 'INIDA' WHERE CustomerID = 4;
+
