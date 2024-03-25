63ee5d512b7a7c738fd910b532c59d1457beaaa8
Rahul Kumar <guptarahulboy123@gmail.com>
2024-03-25 16:10:21 +0530

    add insert, drop, delete,truncate,alter for table

---
+++ SQL/Table Statement.sql
@@ -0,0 +1,30 @@
+-- 1. Create Table
+CREATE TABLE EMPLOYEE(
+     EMP_ID INT,
+     NAME VARCHAR(20),
+     DOB DATE,
+     AGE INT,
+     SALARY DECIMAL(7,2));
+
+-- 2. Insert records into Table
+INSERT INTO EMPLOYEE (EMP_ID,NAME,DOB,AGE,SALARY) VALUES
+(1,"John",'2001-02-23',23,5000.54),
+(2,"John1",'2002-02-23',29,9000.5),
+(3,"John2",'2003-02-23',33,5070.04);
+
+-- 3. Drop(delete) Table
+DROP TABLE Student;   -- student is tablename
+
+-- 4.a Delete records from Table(Single records)
+delete from EMPLOYEE where AGE=29;
+
+-- 4.b Delete records from Table(ALL records)
+delete from EMPLOYEE ;
+-- or
+delete * from EMPLOYEE;
+
+-- 5. Rename Table Name
+ALTER TABLE EMPLOYEE RENAME TO emp_details;
+
+-- 6. Change column name 
+ALTER TABLE emp_details RENAME COLUMN NAME TO EMP_NAME; 
